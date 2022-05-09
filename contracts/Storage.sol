bb0bea1d303c5f78e5257082746f43d984bde9b1
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-10 06:57:51 +0700

    test Storage address

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,6 +2,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Storage {
+    
+    mapping(uint => uint) public aa; // slot 0
+    mapping(address => uint) public bb; // slot 1
+
+    //keccak256(key + slot)
+    
     uint8 public a = 7; //1byte
     uint16 public b = 8; //2 bytes
     address public c = 0x61335f29b1fdA4d86f4cD0062b5d0E3D6E7FC693;
@@ -10,8 +16,19 @@ contract Storage {
     // 32bytes all valus will be storages in slot 0
 
 
-    uint256 public f = 200; //32 bytes -> slot 1
-    uint8 public g = 40; //1 byte -> slot 2
-    uint256 public h = 789; //32 bytes -> slot 3
+    uint256 public f = 200; //32 bytes -> slot 3
+    uint8 public g = 40; //1 byte -> slot 4
+    uint256 public h = 789; //32 bytes -> slot 5
  
+    constructor(){
+        aa[2] = 4;
+        aa[3] = 10;
+
+        bb[0x61335f29b1fdA4d86f4cD0062b5d0E3D6E7FC693] = 100;
+    }
 }
+
+
+//0x0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000
+
+//web3.eth.getStorageAt("")
