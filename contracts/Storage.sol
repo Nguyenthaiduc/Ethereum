048e06f726743f07b8a0f452541d5c9dc2e01aea
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-10 07:05:01 +0700

    add comment and cc array

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -3,24 +3,29 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Storage {
     
+    //keccak256(key + slot)
     mapping(uint => uint) public aa; // slot 0
     mapping(address => uint) public bb; // slot 1
 
-    //keccak256(key + slot)
+    uint[] public cc; // slot 2
+
     
     uint8 public a = 7; //1byte
     uint16 public b = 8; //2 bytes
     address public c = 0x61335f29b1fdA4d86f4cD0062b5d0E3D6E7FC693;
     bool d = true;  //1bytes
     uint64 public e = 15; //8 bytes
-    // 32bytes all valus will be storages in slot 0
+    // 32bytes all valus will be storages in slot 3
 
 
-    uint256 public f = 200; //32 bytes -> slot 3
-    uint8 public g = 40; //1 byte -> slot 4
-    uint256 public h = 789; //32 bytes -> slot 5
+    uint256 public f = 200; //32 bytes -> slot 4
+    uint8 public g = 40; //1 byte -> slot 5
+    uint256 public h = 789; //32 bytes -> slot 6
  
     constructor(){
+        cc.push(1);
+        cc.push(10);
+        cc.push(100);
         aa[2] = 4;
         aa[3] = 10;
 
@@ -30,5 +35,5 @@ contract Storage {
 
 
 //0x0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000
-
+//// 0x000000000000000000000000f8929048D74164582E5FA0897fC654CbF0c096C60000000000000000000000000000000000000000000000000000000000000001
 //web3.eth.getStorageAt("")
